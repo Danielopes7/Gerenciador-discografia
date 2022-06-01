65b0e94c0fe6751cc42cf5cc23e10edb179f3920
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-05-31 22:44:59 -0300

    fix: Adicionando logo Tiao

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,9 +45,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo' => '<b>Tião</b>Carreiro',
+    'logo_img' => 'imagens/tiao_logo.png',
+    'logo_img_class' => 'brand-image img-circle elevation-4',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'AdminLTE',
