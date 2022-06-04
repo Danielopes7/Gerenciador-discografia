1a6ef69e963026de7dbf51db2b657077a778d2f2
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-06-03 22:48:04 -0300

    fix: adicionando relacionamento Hasmany no model Album

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     use HasFactory;
+
+    /**
+     * permito atribuição em massa
+     */
+    protected $fillable = ['nome'];
+
+    /**
+     * retorna todas as faixas de um album.
+     */
+    public function tracks(){
+        return $this->hasMany('App\Models\Track');
+    }
 }
